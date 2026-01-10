4ef598e658b2c9cf7a475ef4bd3e52584ba082c2
Roby047 <robyjaya047@gmail.com>
2026-01-10 17:25:36 +0700

    fix all

--- src/app/medan/about-us/page.tsx
+++ src/app/medan/about-us/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/medan/Header";
+import Footer from "@/components/medan/Footer";
 import AboutSection from "@/components/medan/AboutSection";
 import FloatingWhatsApp from "@/components/medan/FloatingWhatsApp";
 import { MotionDiv } from "@/components/animations/MotionDiv";
