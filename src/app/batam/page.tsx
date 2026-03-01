6952b5def6716328edc751130e38d1fc384d2af4
roby047 <http://roby047.github.com/>
2026-03-02 03:17:26 +0700

    fix tombol

--- src/app/batam/page.tsx
+++ src/app/batam/page.tsx
@@ -168,7 +168,7 @@ export default function HomePage() {
                     Pesan Sekarang
                   </a>
                 </Button>
-                <Button size="lg" variant="outline" className="border-2 border-white text-white hover:bg-white hover:text-teal-700 text-lg px-8" asChild>
+                <Button size="lg" variant="outline" className="bg-transparent border-2 border-white text-white hover:bg-white hover:text-teal-700 text-lg px-8" asChild>
                   <a href="/batam/armada">
                     <Car className="w-5 h-5 mr-2" />
                     Lihat Armada
